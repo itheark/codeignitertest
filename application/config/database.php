4655f03bef8ca7bfe9ed79790c33b085e892b6f2
itheark <itheark@gmail.com>
2017-03-18 14:51:10 +0530

    test

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+//test
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $db['default'] = array(
